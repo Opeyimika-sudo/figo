fb0bb609d8896ebd25eedbad2c6fbab3f3cef8ab
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2023-11-22 10:25:43 +0100

    feat: made the Header responsive

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Logo from './Logo'
+import { Input } from "../Components/ui/input"
+import { Button } from './ui/button'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex gap-x-2 lg:gap-x-6'>
+        <Logo/>
+        <div className='relative w:3/5 lg:w-4/5 flex items-center'>
+            <span className="material-symbols-outlined absolute left-1 lg:left-2 text-xs lg:text-lg">
+                search
+            </span>
+            <Input type="text" placeholder="Search by name" className='font-noto text-xs w:3/5 lg:w-2/4 pl-4 lg:pl-10'/>
+        </div>
+        <Button className='bg-teal-600 rounded-lg'>Add a photo</Button>
+    </div>
   )
 }
 
--- frontend/src/Components/Logo.tsx
+++ frontend/src/Components/Logo.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div>Logo</div>
+    <div className='flex items-center gap:x-0.5 lg:gap-x-2'>
+        <span className="material-symbols-outlined">
+        person
+        </span>
+        <div>
+            <h2 className='font-medium font-noto text-sm md:text-base lg:text-xl'>Figo</h2>
+            <p className='font-medium font-noto text-xs'>devchallenges.io</p>
+        </div>
+    </div>
   )
 }
 
