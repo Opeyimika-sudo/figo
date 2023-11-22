da90fa8154f172bdc03c5925368cc4f376a6b37e
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2023-11-22 10:55:53 +0100

    feat: display images received from backend

--- frontend/src/Components/Main.tsx
+++ frontend/src/Components/Main.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import axios from 'axios'
+
+interface Image {
+    id: number,
+    image_url: string,
+    image_desc: string,
+    created_on: string
+}
 
 const Main = () => {
+    const [images, setImages] = React.useState<Image[]>([])
+
+    React.useEffect(() => {
+        axios('http://127.0.0.1:5000/').then(response => setImages(response.data)).catch(error => console.log(error))
+    }, [])
+
+    console.log(images);
+
+    const imageWall = images.map(item => (
+        <img src={item.image_url} alt={item.image_desc}/>
+    ))
   return (
-    <div></div>
+    <div>{imageWall}</div>
   )
 }
 
