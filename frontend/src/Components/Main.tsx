2f8447c1ff976b1d504d606da5ca4cf4e2ee4b53
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2023-11-22 23:38:12 +0100

    feat: show delete button only when image is hovered upon

--- frontend/src/Components/Main.tsx
+++ frontend/src/Components/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 // import Masonry from 'react-masonry-component'
 import MasonryComponent from './MasonryComponent'
+import { Button } from './ui/button'
 
 interface Image {
     id: number,
@@ -17,11 +18,11 @@ const Main = () => {
         axios('http://127.0.0.1:5000/').then(response => setImages(response.data)).catch(error => console.log(error))
     }, [])
 
-    console.log(images);
-
     const imageWall = images.map(item => (
-        <div className=''>
-            <img key={item.id} src={item.image_url} alt={item.image_desc} className='object-contain'/>
+        <div className='relative'>
+            <img key={item.id} src={item.image_url} alt={item.image_desc} className='object-contain rounded-2xl cursorChange'/>
+            <Button className='absolute right-2 top-2 border-red-500 border-2 text-red-500 bg-inherit px-2 py-0.5 rounded-2xl button'>delete</Button>
+            <p></p>
         </div>
     ))
 
