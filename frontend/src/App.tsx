143d8219b33a0ed2a6dec0e1a29b578a8965b1d8
Opeyimika Aremu <opeyimikaaremu@gmail.com>
2023-12-10 01:58:03 +0100

    feat: install react-toastify to send toast notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,18 @@ import './App.css'
 import Header from './Components/Header'
 import Main from './Components/Main'
 import ImageContextProvider from './Components/ImageContextProvider'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
-  
+
   return (
     <div className="mx-3 lg:mx-10 my-3">
       <ImageContextProvider>
         <Header/>
         <Main/>
+        <ToastContainer />
       </ImageContextProvider>
     </div>
   )
